3479f413d4c17a8a394fb615155e78e0ab2ed3a0
mr1fatq <revoy44589@ovooovo.comm>
2022-03-10 23:17:12 +0200

    C learning

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	fprintf(stderr, 'and that piece of art is useful" - Dora Korpar, 2015-10-1\n);
+	fprintf(stderr, 'and that piece of art is useful" - Dora Korpar, 2015-10-1\n');
 	return (1);
 }
