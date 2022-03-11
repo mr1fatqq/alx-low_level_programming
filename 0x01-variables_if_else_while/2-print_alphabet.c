ff5b5731a663ca9ce254a3afdd59b062d4a2455f
mr1fatq <revoy44589@ovooovo.comm>
2022-03-11 23:25:01 +0200

    C variables and keywords

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,21 @@
-#include <stdio.h>
+include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always retun 0
  */
+
 int main(void)
 {
-	char low;
+	char c;
 
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-		printf("\n");
-
+	c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
 	return (0);
 }
