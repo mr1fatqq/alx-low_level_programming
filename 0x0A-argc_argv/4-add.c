f1cec5cd4a7ccd6bf0fc62fed6418d457d9aa177
mr1fatq <revoy44589@ovooovo.comm>
2022-04-02 01:23:55 +0300

    C arg

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,31 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "main.h"
 /**
- * main - the entry point
- * @argc: argument count
- * @argv: argument vector
- * Return: always return 1
+ * main - Entry Point
+ * @argc: arguments
+ * @argv: array pointing to arguments
+ * Return: 0
  */
-
 int main(int argc, char *argv[])
 {
-	int i;
-	int sum = 0;
+	int i, sum = 0;
 
-	if (argc > 1)
+	if (argc < 1)
+		return (0);
+
+	for (i = 1; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
+		if (!atoi(argv[i]))
 		{
-			sum += atoi(argv[i]);
+			printf("%s\n", "Error");
+			return (1);
 		}
-		printf("%d\n", sum);
+		sum += atoi(argv[i]);
 	}
-	else
-	{
-		printf("0\n");
-		return (1);
-	}
-	return (0);
+	printf("%d\n", sum);
 
+	return (0);
 }
